9e5c5b39099e7789ed721effd3ddc9b54bef9496
habiblawal1 <habiblawal@hotmail.co.uk>
2023-01-08 20:48:43 +0000

    Server to print associated hostname

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const proxyHost = req.headers["x-forwarded-host"];
+  const host = proxyHost ? proxyHost : req.headers.host;
+  res.send(`Test: ${host}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
